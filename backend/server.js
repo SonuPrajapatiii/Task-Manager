2aad7d0113535208137326035b75c14380c00bbd
﻿Sonu <sonup5018@gmail.com>
2026-05-16 19:21:13 +0530

    env updated

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(cors());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/taskmanager')
+mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('MongoDB connection error:', err));
 
